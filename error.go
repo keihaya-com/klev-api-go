4ed2f0cdf6a7734927bd10664a262b0bb4253b22
Nikolay Petrov <sungon@gmail.com>
2022-07-12 21:44:54 -0400

    make errors actual errors

--- error.go
+++ error.go
@@ -1,7 +1,12 @@
 package api
 
+import "fmt"
+
 type ErrorOut struct {
-	Error   bool   `json:"error,omitempty"`
 	Code    string `json:"code,omitempty"`
 	Message string `json:"message,omitempty"`
+}
+
+func (e ErrorOut) Error() string {
+	return fmt.Sprintf("(%s) %s", e.Code, e.Message)
 }
