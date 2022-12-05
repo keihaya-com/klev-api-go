2c2bc84483cb18026ea3b495aec0b670fa62be89
Nikolay Petrov <sungon@gmail.com>
2022-12-05 08:57:02 -0500

    extract acl to separate file

---
+++ acl.go
@@ -0,0 +1,35 @@
+package api
+
+import "fmt"
+
+type Subject string
+
+var (
+	SubjectLogs     Subject = "logs"
+	SubjectTokens   Subject = "tokens"
+	SubjectWebhooks Subject = "webhooks"
+)
+
+type Action string
+
+var (
+	ActionList   Action = "list"
+	ActionCreate Action = "create"
+	ActionGet    Action = "get"
+	ActionDelete Action = "delete"
+
+	ActionPublish Action = "publish"
+	ActionConsume Action = "consume"
+)
+
+func ACLSubject(subject Subject) string {
+	return fmt.Sprintf("%s", subject)
+}
+
+func ACLAction(subject Subject, action Action) string {
+	return fmt.Sprintf("%s:%s", subject, action)
+}
+
+func ACLObject(subject Subject, action Action, id string) string {
+	return fmt.Sprintf("%s:%s:%s", subject, action, id)
+}
--- tokens.go
+++ tokens.go
@@ -23,26 +23,6 @@ type TokenOut struct {
 	Bearer   string   `json:"bearer,omitempty"`
 }
 
-type Subject string
-
-var (
-	SubjectLogs     Subject = "logs"
-	SubjectTokens   Subject = "tokens"
-	SubjectWebhooks Subject = "webhooks"
-)
-
-type Action string
-
-var (
-	ActionList   Action = "list"
-	ActionCreate Action = "create"
-	ActionGet    Action = "get"
-	ActionDelete Action = "delete"
-
-	ActionPublish Action = "publish"
-	ActionConsume Action = "consume"
-)
-
 func (c *Client) TokensList(ctx context.Context) ([]TokenOut, error) {
 	var out TokensOut
 	err := c.HTTPGet(ctx, fmt.Sprintf("tokens"), &out)
@@ -64,16 +44,4 @@ func (c *Client) TokenGet(ctx context.Context, id TokenID) (TokenOut, error) {
 func (c *Client) TokenDelete(ctx context.Context, id TokenID) error {
 	var out TokenOut
 	return c.HTTPDelete(ctx, fmt.Sprintf("token/%s", id), &out)
-}
-
-func (c *Client) ACLSubject(subject Subject) string {
-	return fmt.Sprintf("%s", subject)
-}
-
-func (c *Client) ACLAction(subject Subject, action Action) string {
-	return fmt.Sprintf("%s:%s", subject, action)
-}
-
-func (c *Client) ACLObject(subject Subject, action Action, id string) string {
-	return fmt.Sprintf("%s:%s:%s", subject, action, id)
 }
