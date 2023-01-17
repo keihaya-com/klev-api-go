ee5c83f6fd6ef6e1beca12b861163720bcbaaadc
Nikolay Petrov <sungon@gmail.com>
2023-01-16 20:49:32 -0500

    update offset values

--- consume.go
+++ consume.go
@@ -11,10 +11,12 @@ import (
 const (
 	// OffsetOldest represents the smallest offset still available
 	// Use it to consume all messages, starting at the beginning of the log
-	OffsetOldest int64 = -1
+	OffsetOldest int64 = -2
 	// OffsetNewest represents the offset that will be used for the next produce
 	// Use it to consume messages, starting from the next one produced
-	OffsetNewest int64 = -2
+	OffsetNewest int64 = -1
+	// OffsetInvalid is the offset returned when error is detected
+	OffsetInvalid int64 = -3
 )
 
 type ConsumeOut struct {
